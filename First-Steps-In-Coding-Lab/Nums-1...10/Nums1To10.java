b922fdb9f5a945c0db8ef98b553bf7001f38e85f
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-07 11:18:47 +0200

    Create Nums1To10.java

---
+++ First-Steps-In-Coding-Lab/Nums-1...10/Nums1To10.java
@@ -0,0 +1,12 @@
+// Напишете Java конзолна програма, която отпечатва числата от 1 до 10 на отделни редове на конзолата.
+
+package SoftUni;
+
+import static java.lang.System.out;
+
+public class Nums1To10 {
+    public static void main(String[] args) {
+        for(int i = 1; i < 11; i++)
+            out.println(i);
+    }
+}

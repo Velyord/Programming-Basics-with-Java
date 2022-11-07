0427b6829b81262e39d197f09f6816a646d130c4
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-07 11:16:39 +0200

    Create Hello-SoftUni.java

---
+++ First-Steps-In-Coding-Lab/Hello-SoftUni.java
@@ -0,0 +1,12 @@
+// Конзолна програма "Hello SoftUni"
+// Напишете конзолна Java програма, която отпечатва текста "Hello SoftUni".
+
+package SoftUni;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        out.println("Hello SoftUni");        
+    }
+}

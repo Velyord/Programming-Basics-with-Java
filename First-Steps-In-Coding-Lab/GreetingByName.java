0f3a718cb046f8b71383ed7b4ca39d99876b68ba
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-07 11:23:48 +0200

    Create GreetingByName.java

---
+++ First-Steps-In-Coding-Lab/GreetingByName.java
@@ -0,0 +1,14 @@
+// Да се напише програма, която чете от конзолата текст (име на човек) и отпечатва "Hello, <name>!", където <name> е въведеното име от конзолата.
+
+package SoftUni;
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class GreetingByName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        out.printf("Hello, %s!", name);
+    }
+}

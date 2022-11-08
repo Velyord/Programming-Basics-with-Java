9581cbe06605b6c6267ea485d0e556843f05c327
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-08 17:55:32 +0200

    Update RadiansToDegrees.java

--- First Steps-In-Coding-Exercise/RadiansToDegrees.java
+++ First Steps-In-Coding-Exercise/RadiansToDegrees.java
@@ -1,22 +1,44 @@
-// Напишете програма, която чете ъгъл в радиани (десетично число) и го преобразува в градуси. Използвайте формулата: градус = радиан * 180 / π. Числото π в Java програми е достъпно чрез Math.PI.
-    // Примерен вход и изход
-        // 3.1416 -> 180.0004209182994
-        // 6.2832 -> 360.0008418365988
-        // 0.7854 -> 45.00010522957485
-
-package Exe4;
+/*
+Условие:
+    Напишете програма, която чете ъгъл в радиани (десетично число) и го преобразува в градуси. Използвайте формулата: градус = радиан * 180 / π. Числото π в Java програми е достъпно чрез Math.PI.
+Примерен вход и изход:
+    3.1416 -> 180.0004209182994
+    6.2832 -> 360.0008418365988
+    0.7854 -> 45.00010522957485
+*/
+package SoftUni.Exer4;
 
 import java.util.Scanner;
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class RadiansToDegrees {
+    static Scanner scanner = new Scanner(in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double rad = Double.parseDouble(scanner.nextLine());
-        double angleToRad = angleToRad(rad);
-        out.println(angleToRad);
+        double rads = setDoubleValue();
+        
+        double angles = radsToAngles(rads);
+        out.println(angles);
     }
-    private static double angleToRad(double rad) {
-        return rad * 180/Math.PI;
+    private static double radsToAngles(double rads) {
+        return rads * 180 / Math.PI;
+    }
+    private static double setDoubleValue() {
+        double value;
+
+        try {
+            value = Double.parseDouble(scanner.nextLine());
+        }
+        catch (Exception e){
+            out.println("Не сте въвели число. Пробвайте пак!");
+            return setDoubleValue();
+        }
+        if (value < 0) {
+            out.println("Моля въведе положително число");
+            return setDoubleValue();
+        }
+        else
+            return value;
     }
 }

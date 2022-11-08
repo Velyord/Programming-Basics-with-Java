56b93201fa118880a83ec017c93ff4fe37f07ed7
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-08 15:56:26 +0200

    Update RectangleArea.java

--- First-Steps-In-Coding-Lab/RectangleArea.java
+++ First-Steps-In-Coding-Lab/RectangleArea.java
@@ -1,15 +1,38 @@
-// Да се напише конзолна програма, която въвежда две цели числа (страните на правоъгълника a и b) и пресмята лицето на правоъгълник с тези страни.
-
-package SoftUni;
+/*
+Условие:
+    Да се напише конзолна програма, която въвежда две цели числа (страните на правоъгълника a и b) и пресмята лицето на правоъгълник с тези страни.
+*/
+package SoftUni.Lab3;
 
 import static java.lang.System.out;
 import java.util.Scanner;
 
 public class RectangleArea {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = Integer.parseInt(scanner.nextLine());
-        int b = Integer.parseInt(scanner.nextLine());
-        out.println(a*b);
+        int a = setSide();
+        int b = setSide();
+
+        int area = calculateRectangleArea(a, b);
+        out.println(area);
+    }
+    private static int setSide() {
+        int side;
+
+        try {
+            side = Integer.parseInt(scanner.nextLine());
+        } catch (Exception e){
+            out.println("Не сте въвели число. Пробвайте пак!");
+            return setSide();
+        }
+        if (side < 0) {
+            out.println("Моля въведе положително число!");
+            return setSide();
+        } else
+            return side;
+    }
+    private static int calculateRectangleArea(int a, int b) {
+        return a * b;
     }
 }

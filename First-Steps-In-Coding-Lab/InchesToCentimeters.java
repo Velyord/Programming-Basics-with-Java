d9c6b35e2a71fdf14d706fb2086dbee58802b6f5
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-08 11:48:27 +0200

    Update InchesToCentimeters.java

--- First-Steps-In-Coding-Lab/InchesToCentimeters.java
+++ First-Steps-In-Coding-Lab/InchesToCentimeters.java
@@ -1,6 +1,8 @@
-// Да се напише програма, която чете от конзолата реално число и го преобразува от инчове в сантиметри. За целта умножете инчовете по 2.54 (1 инч = 2.54 сантиметра).
-
-package SoftUni;
+/*
+Условие:
+    Да се напише програма, която чете от конзолата реално число и го преобразува от инчове в сантиметри. За целта умножете инчовете по 2.54 (1 инч = 2.54 сантиметра).
+*/
+package SoftUni.Lab3;
 
 import static java.lang.System.out;
 import java.util.Scanner;
@@ -8,7 +10,9 @@ import java.util.Scanner;
 public class InchesToCentimeters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         double input = Double.parseDouble(scanner.nextLine());
+        
         out.println(input*2.54);
     }
 }

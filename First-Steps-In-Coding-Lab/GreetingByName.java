2a97baabaa04a8a98d3f0a559b1e840c2ac58efc
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-14 16:55:47 +0200

    Update GreetingByName.java

--- First-Steps-In-Coding-Lab/GreetingByName.java
+++ First-Steps-In-Coding-Lab/GreetingByName.java
@@ -5,14 +5,34 @@
 package SoftUni.Lab3;
 
 import static java.lang.System.out;
+import static java.lang.System.in;
 import java.util.Scanner;
 
 public class GreetingByName {
+    static Scanner scanner = new Scanner(in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        String name = scanner.nextLine();
-        
+        String name = setStringValue();
+
         out.printf("Hello, %s!", name);
+    }
+
+    private static String setStringValue() {
+        String value;
+        value = scanner.nextLine();
+        boolean isSpecChar = false;
+        String specialCharacters="!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+
+        for (int i = 0; i < value.length(); i++)
+            if (specialCharacters.contains(Character.toString(value.charAt(i)))) {
+                isSpecChar = true;
+            }
+
+        if(isSpecChar) {
+            out.println("Моля въведете правилно име!");
+            return setStringValue();
+        }
+        else
+            return value;
     }
 }

bb800c5f245e30efb498236b32c06c8106105a45
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-14 17:14:13 +0200

    Update ProjectsCreation.java

--- First-Steps-In-Coding-Lab/ProjectsCreation.java
+++ First-Steps-In-Coding-Lab/ProjectsCreation.java
@@ -19,34 +19,64 @@
 package SoftUni.Lab3;
 
 import static java.lang.System.out;
+import static java.lang.System.in;
 import java.util.Scanner;
 
 public class ProjectsCreation {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
-        String archName = scanner.nextLine();
-        int numOfProj = setNumOfProj();
+        String archName = setStringValue();
+        int numOfProj = setIntValue(0, 100);
 
-        int hoursForProj = 3;
-        int sumOfHours = numOfProj * hoursForProj;
+        int sumOfHours = calculateSumOfHours(numOfProj);
 
         String output = "The architect %s will need %d hours to complete %d project/s.";
         out.printf(output, archName, sumOfHours, numOfProj);
     }
-    private static int setNumOfProj() {
-        int numOfProj;
+
+    private static int calculateSumOfHours(int numOfProj) {
+        int hoursForProj = 3;
+
+        return numOfProj * hoursForProj;
+    }
+
+    private static int setIntValue(int min, int max) {
+        int value;
 
         try {
-            numOfProj = Integer.parseInt(scanner.nextLine());
-        } catch (Exception e){
-            out.println("Не сте въвели число. Пробвайте пак!");
-            return setNumOfProj();
+            value = Integer.parseInt(scanner.nextLine());
         }
-        if (numOfProj < 0 || numOfProj > 100) {
-            out.println("Моля въведе число между 0 и 100!");
-            return setNumOfProj();
-        } else
-            return numOfProj;
+        catch (Exception e){
+            out.println("Не сте въвели число. Пробвайте пак!");
+            return setIntValue(min, max);
+        }
+
+        if (value < min || value > max) {
+            out.printf("Моля въведе число между %d и %d!", min, max);
+            return setIntValue(min, max);
+        }
+        else
+            return value;
+    }
+
+    private static String setStringValue() {
+        String value;
+        value = scanner.nextLine();
+
+        boolean isSpecChar = false;
+        String specialCharacters="!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+
+        for (int i = 0; i < value.length(); i++)
+            if (specialCharacters.contains(Character.toString(value.charAt(i)))) {
+                isSpecChar = true;
+            }
+
+        if(isSpecChar) {
+            out.println("Моля въведете правилно име!");
+            return setStringValue();
+        }
+        else
+            return value;
     }
 }

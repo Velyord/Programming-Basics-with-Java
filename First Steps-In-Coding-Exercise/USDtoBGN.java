b14e84410fcb6330a07e1679337724224f6b93c7
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-14 16:16:06 +0200

    Update USDtoBGN.java

--- First Steps-In-Coding-Exercise/USDtoBGN.java
+++ First Steps-In-Coding-Exercise/USDtoBGN.java
@@ -1,25 +1,52 @@
-// Напишете програма за конвертиране на щатски долари (USD) в български лева (BGN). Използвайте фиксиран курс между долар и лев: 1 USD = 1.79549 BGN.
-    // Примерен вход и изход
-        // 22 -> 39.50078 
-        // 100 -> 179.549
-        // 12.5 -> 22.443625
-
-package Exe4;
+/*
+Условие:
+    Напишете програма за конвертиране на щатски долари (USD) в български лева (BGN). Използвайте фиксиран курс между долар и лев: 1 USD = 1.79549 BGN.
+Примерен вход и изход:
+    22
+    -> 39.50078
+    100
+    -> 179.549
+    12.5
+    -> 22.443625
+*/
+package SoftUni.Exer4;
 
 import java.util.Scanner;
-
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class USDtoBGN {
+    static Scanner scanner = new Scanner(in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         double kurs = 1.79549;
-        double bgn = Double.parseDouble(scanner.nextLine());
+
+        out.println("Въведи сума български пари: ");
+        double bgn = setDoubleValue();
+
         double bgnToUsd = convertBGNtoUSD(bgn, kurs);
         out.println(bgnToUsd);
     }
 
     private static double convertBGNtoUSD(double bgn, double kurs) {
-        return bgn*kurs;
+        return bgn * kurs;
+    }
+
+    private static double setDoubleValue() {
+        double value;
+
+        try {
+            value = Double.parseDouble(scanner.nextLine());
+        }
+        catch (Exception e){
+            out.println("Не сте въвели число. Пробвайте пак!");
+            return setDoubleValue();
+        }
+        if (value < 0) {
+            out.println("Моля въведе положително число");
+            return setDoubleValue();
+        }
+        else
+            return value;
     }
 }

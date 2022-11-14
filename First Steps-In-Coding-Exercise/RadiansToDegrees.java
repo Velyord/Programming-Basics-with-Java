40ac191e40d80ecf520608f3fe324d54639d492f
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-14 17:58:45 +0200

    Update RadiansToDegrees.java

--- First Steps-In-Coding-Exercise/RadiansToDegrees.java
+++ First Steps-In-Coding-Exercise/RadiansToDegrees.java
@@ -8,22 +8,25 @@
 */
 package SoftUni.Exer4;
 
-import java.util.Scanner;
-import static java.lang.System.in;
 import static java.lang.System.out;
+import static java.lang.System.in;
+import java.util.Scanner;
 
 public class RadiansToDegrees {
     static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
         double rads = setDoubleValue();
-        
+
         double angles = radsToAngles(rads);
+
         out.println(angles);
     }
+
     private static double radsToAngles(double rads) {
         return rads * 180 / Math.PI;
     }
+
     private static double setDoubleValue() {
         double value;
 

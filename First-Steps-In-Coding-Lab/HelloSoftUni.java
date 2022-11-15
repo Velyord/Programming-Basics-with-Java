472b6da5a8371ed87164e0be4ac6a1b8c5a51383
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-15 09:39:53 +0200

    Update HelloSoftUni.java

--- First-Steps-In-Coding-Lab/HelloSoftUni.java
+++ First-Steps-In-Coding-Lab/HelloSoftUni.java
@@ -6,8 +6,8 @@ package SoftUni.Lab3;
 
 import static java.lang.System.out;
 
-public class Main {
+public class HelloSoftUni {
     public static void main(String[] args) {
-        out.println("Hello SoftUni");        
+        out.println("Hello SoftUni");
     }
 }

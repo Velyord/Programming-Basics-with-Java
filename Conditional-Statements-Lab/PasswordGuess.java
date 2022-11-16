0daf0bca6720fee59d24eb298d6413f1d7a0bc3d
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-16 17:01:34 +0200

    Create PasswordGuess.java

---
+++ Conditional-Statements-Lab/PasswordGuess.java
@@ -0,0 +1,36 @@
+/*
+Условие:
+    Да се напише програма, която чете парола (текст), въведена от потребителя и проверява дали въведената парола съвпада с фразата "s3cr3t!P@ssw0rd". При съвпадение да се изведе "Welcome". При несъвпадение да се изведе "Wrong password!".
+Примерен вход и изход:
+    qwerty
+    -> Wrong password!
+    s3cr3t!P@ssw0rd
+    -> Welcome
+    s3cr3t!p@ss
+    -> Wrong password!
+
+ */
+package SoftUni.Lab5;
+
+import static java.lang.System.out;
+import static java.lang.System.in;
+import java.util.Scanner;
+
+public class PasswordGuess {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        String password = scanner.nextLine();
+
+        String isCorrect = checkPassword(password);
+
+        out.println(isCorrect);
+    }
+
+    private static String checkPassword(String password) {
+        if (password.equals("s3cr3t!P@ssw0rd"))
+            return "Welcome";
+        else
+            return "Wrong password!";
+    }
+}

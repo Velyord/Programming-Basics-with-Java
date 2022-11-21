9862ea02e913bd7c300eefe4093b0979babe2b99
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-21 17:26:21 +0200

    Update and rename Conditional-Statements-Advanced/DayOfWeek.java to Conditional-Statements-Advanced-Lab/DayOfWeek.java

--- Conditional-Statements-Advanced-Lab/DayOfWeek.java
+++ Conditional-Statements-Advanced-Lab/DayOfWeek.java
@@ -4,14 +4,22 @@
     (на английски език), в граници [1...7] или отпечатва "Error" в случай, че въведеното число е
     невалидно.
 Примерен вход и изход"
-    1 -> Monday
-    2 -> Tuesday
-    3 -> Wednesday
-    4 -> Thursday
-    5 -> Friday
-    6 -> Saturday
-    7 -> Sunday
-    -1 -> Error
+    1
+    -> Monday
+    2
+    -> Tuesday
+    3
+    -> Wednesday
+    4
+    -> Thursday
+    5
+    -> Friday
+    6
+    -> Saturday
+    7
+    -> Sunday
+    -1
+    -> Error
  */
 package SoftUni.Lab7;
 
@@ -26,14 +34,14 @@ public class DayOfWeek {
     public static void main(String[] args) {
         int day = Integer.parseInt(scanner.nextLine());
         switch (day) {
-            case 1: out.println("Monday");      break;
-            case 2: out.println("Tuesday");     break;
-            case 3: out.println("Wednesday");   break;
-            case 4: out.println("Thursday");    break;
-            case 5: out.println("Friday");      break;
-            case 6: out.println("Saturday");    break;
-            case 7: out.println("Sunday");      break;
-            default: out.println("Error");      break;
+            case 1:     out.println("Monday");      break;
+            case 2:     out.println("Tuesday");     break;
+            case 3:     out.println("Wednesday");   break;
+            case 4:     out.println("Thursday");    break;
+            case 5:     out.println("Friday");      break;
+            case 6:     out.println("Saturday");    break;
+            case 7:     out.println("Sunday");      break;
+            default:    out.println("Error");       break;
         }
     }
 }

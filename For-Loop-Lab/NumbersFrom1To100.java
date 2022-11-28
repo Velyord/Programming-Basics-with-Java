f04a45072724c165082c17ae75ee89005e08f6d5
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-28 21:37:02 +0200

    Update NumbersFrom1To100.java

--- For-Loop-Lab/NumbersFrom1To100.java
+++ For-Loop-Lab/NumbersFrom1To100.java
@@ -1,2 +1,23 @@
 /*
-*/
+Условие:
+    Напишете програма, която отпечатва числата от 1 до 100, всяко на нов ред.
+Примерен вход и изход:
+    (няма) ->
+    1
+    2
+    3
+    …
+    98
+    99
+    100
+ */
+package SoftUni.Lab9;
+
+import static java.lang.System.out;
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++)
+            out.println(i);
+    }
+}

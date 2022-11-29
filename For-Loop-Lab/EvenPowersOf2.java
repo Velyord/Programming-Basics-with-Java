b7f0c52e7bc7abe30121aba6a92e2fa764f5a9c9
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-11-29 21:08:34 +0200

    Create EvenPowersOf2.java

---
+++ For-Loop-Lab/EvenPowersOf2.java
@@ -0,0 +1,69 @@
+/*
+Условие:
+    Напишете програма, която чете число n, въведено от потребителя и отпечатва числата от 1 до n през 3.
+Примерен вход и изход:
+    10
+    -> 1
+    -> 4
+    -> 7
+    -> 10
+    7
+    -> 1
+    -> 4
+    -> 7
+    15
+    -> 1
+    -> 4
+    -> 7
+    -> 10
+    -> 13
+ */
+package SoftUni.Lab9;
+
+import static java.lang.Math.pow;
+import static java.lang.System.out;
+import static java.lang.System.in;
+import java.util.Scanner;
+
+public class EvenPowersOf2 {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int number = setValue("int", true);
+
+        for (int i=0; i<=number; i++)
+            if (i % 2 == 0)
+                out.printf("%.0f\n", pow(2, i));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(String type, Boolean positiveOnly) {
+        Object value;
+        // out.println("Въведете :");
+
+        try {
+            if (type.equals("int"))
+                value = Integer.parseInt(scanner.nextLine());
+            else
+                value = Double.parseDouble(scanner.nextLine());
+        } catch (Exception e) {
+            out.println("Не сте въвели число. Пробвайте пак!");
+            return setValue(type, positiveOnly);
+        }
+
+        if (type.equals("int") && positiveOnly) {
+            if ((int) value < 0) {
+                out.println("Моля въведете положително число между");
+                return setValue(type, positiveOnly);
+            }
+        }
+        if (type.equals("double") && positiveOnly){
+            if ((double) value < 0) {
+                out.println("Моля въведете положително число между");
+                return setValue(type, positiveOnly);
+            }
+        }
+
+        return (T) value;
+    }
+}

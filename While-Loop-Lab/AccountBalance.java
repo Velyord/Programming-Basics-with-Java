cec4e59f73336aab40fbea28f8f85cbfa33d44e8
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-04 15:11:17 +0200

    Update AccountBalance.java

--- While-Loop-Lab/AccountBalance.java
+++ While-Loop-Lab/AccountBalance.java
@@ -36,12 +36,11 @@ public class AccountBalance {
 
     public static void main(String[] args) {
         double sum = 0;
-        
+
         while (true) {
             String payment = scanner.nextLine();
             if (!payment.equals("NoMoreMoney")) {
                 double paymentIntoNumber = Double.parseDouble(payment);
-
                 if (paymentIntoNumber >= 0) {
                     out.printf("Increase: %.2f\n", paymentIntoNumber);
                     sum += paymentIntoNumber;
@@ -55,68 +54,5 @@ public class AccountBalance {
                 break;
             }
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> T setValue(T min, T max) {
-        Object value;
-        // out.println("Въведете :");
-
-        if (min == null && max == null) {
-            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
-            boolean isSpecChar = false;
-            value = scanner.nextLine();
-
-            for (int i = 0; i < ((String) value).length(); i++)
-                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
-                    isSpecChar = true;
-                    break;
-                }
-
-            if (isSpecChar) {
-                out.println("Моля въведете правилно наименование!");
-                return setValue(null, null);
-            }
-            /*
-            String[] required = { "" };
-            List<String> requiredList = List.of(required);
-
-            if (!requiredList.contains(value)){
-                out.print("Моля въведете един от следните избори: | ");
-                for (String thing : required)
-                    out.print(thing + " | ");
-
-                return setValue(null, null);
-            } */
-        } else {
-            try {
-                if (max instanceof Integer)
-                    value = Integer.parseInt(scanner.nextLine());
-                else if (max instanceof Double)
-                    value = Double.parseDouble(scanner.nextLine());
-                else {
-                    out.println("Грешка! Не сте въвели правилен тип. Възможности [int, double]");
-                    value = null;
-                    exit(1);
-                }
-            } catch (Exception e) {
-                out.println("Не сте въвели число. Пробвайте пак!");
-                return setValue(min, max);
-            }
-
-            if (max instanceof Integer) {
-                if ((int) value < (int) min || (int) value > (int) max) {
-                    out.println("Моля въведете положително число между");
-                    return setValue(min, max);
-                }
-            }
-            if (max instanceof Double) {
-                if ((double) value < (double) min || (double) value > (double) max) {
-                    out.println("Моля въведете положително число между");
-                    return setValue(min, max);
-                }
-            }
-        }
-        return (T) value;
     }
 }

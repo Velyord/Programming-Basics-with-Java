5ef656bda26964e5f4f6900c984579a61faa00f7
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-04 14:19:00 +0200

    Create SumNumbers.java

---
+++ While-Loop-Lab/SumNumbers.java
@@ -0,0 +1,113 @@
+/*
+Условие:
+    Напишете програма, която чете цяло число от конзолата и на всеки следващ ред цели числа, докато тяхната сума стане
+    по-голяма или равна на първоначалното число. След приключване на четенето да се отпечата сумата на въведените числа.
+Примерен вход и изход:
+    100
+    10
+    20
+    30
+    40
+    -> 100
+    20
+    1
+    2
+    3
+    4
+    5
+    6
+    -> 21
+ */
+package SoftUni.Lab11;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class SumNumbers {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int apexNumber = setValue(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+        reachApexNumber(apexNumber);
+    }
+
+    private static void reachApexNumber(int apexNumber) {
+        int num = setValue(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        int sum = num;
+
+        while (sum < apexNumber) {
+            num = setValue(Integer.MIN_VALUE, Integer.MAX_VALUE);
+            sum += num;
+        }
+
+        out.println(sum);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+            /*
+            String[] required = { "" };
+            List<String> requiredList = List.of(required);
+
+            if (!requiredList.contains(value)){
+                out.print("Моля въведете един от следните избори: | ");
+                for (String thing : required)
+                    out.print(thing + " | ");
+
+                return setValue(null, null);
+            } */
+        } else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка! Не сте въвели правилен тип. Възможности [int, double]");
+                    value = null;
+                    exit(1);
+                }
+            } catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+        }
+        return (T) value;
+    }
+}

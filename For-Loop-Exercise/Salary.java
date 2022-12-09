2319f31e17e05e0d0d2eb98e2b3f225115adf0d3
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-09 17:08:11 +0200

    Update Salary.java

--- For-Loop-Exercise/Salary.java
+++ For-Loop-Exercise/Salary.java
@@ -120,12 +120,13 @@ public class Salary {
             }
         } else {
             try {
+            try {
                 if (max instanceof Integer)
                     value = Integer.parseInt(scanner.nextLine());
                 else if (max instanceof Double)
                     value = Double.parseDouble(scanner.nextLine());
                 else {
-                    out.println("Грешка! Не сте въвели правилен тип. Възможности [int, double]");
+                    out.println("Грешка!");
                     value = null;
                     exit(1);
                 }
@@ -136,13 +137,21 @@ public class Salary {
 
             if (max instanceof Integer) {
                 if ((int) value < (int) min || (int) value > (int) max) {
-                    out.println("Моля въведете положително число между");
+                    if ((int) min == 0 && (int) max == Double.MAX_VALUE)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
                     return setValue(min, max);
                 }
             }
             if (max instanceof Double) {
                 if ((double) value < (double) min || (double) value > (double) max) {
-                    out.println("Моля въведете положително число между");
+                    if ((double) min == 0 && (double) max == Double.MAX_VALUE)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
                     return setValue(min, max);
                 }
             }

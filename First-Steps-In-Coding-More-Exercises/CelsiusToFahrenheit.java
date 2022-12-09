1146c7960e3b5450ab791ad872674ec5b9595fcb
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-09 16:30:12 +0200

    Create CelsiusToFahrenheit.java

---
+++ First-Steps-In-Coding-More-Exercises/CelsiusToFahrenheit.java
@@ -0,0 +1,102 @@
+/*
+Условие:
+    Напишете програма, която чете градуси по скалата на Целзий (°C) и ги преобразува до градуси по
+    скалата на Фаренхайт (°F). Потърсете в Интернет подходяща формула,
+    с която да извършите изчисленията. Форматирате изхода до втория знак след десетичната запетая.
+Примерен вход и изход:
+    25
+    -> 77.00
+    0
+    -> 32.00
+    -5.5
+    -> 22.10
+    32.3
+    -> 90.14
+*/
+package SoftUni.MoreExercises.FirstStepsInCoding;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class CelsiusToFahrenheit {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        double celsius = setValue(Double.MIN_VALUE, Double.MAX_VALUE);
+
+        double fahrenheit = convertCelsiusToFahrenheit(celsius);
+
+        out.println(fahrenheit);
+    }
+
+    private static double convertCelsiusToFahrenheit(double celsius) {
+        return celsius * 1.8 + 32;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+            /*
+            String[] required = { "" };
+            List<String> requiredList = List.of(required);
+            if (!requiredList.contains(value)){
+                out.print("Моля въведете един от следните избори: | ");
+                for (String thing : required)
+                    out.print(thing + " | ");
+                out.println();
+                return setValue(null, null);
+            } */
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка! Не сте въвели правилен тип. Възможности [int, double]");
+                    value = null;
+                    exit(1);
+                }
+            } catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+        }
+        return (T) value;
+    }
+}

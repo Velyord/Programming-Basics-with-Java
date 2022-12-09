bc2ab661a863ddfd8be86c4bad8a863e7de97890
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-09 16:22:55 +0200

    Create TriangleArea.java

---
+++ First-Steps-In-Coding-More-Exercises/TriangleArea.java
@@ -0,0 +1,107 @@
+/*
+Условие:
+    Напишете програма, която чете от конзолата страна и височина на триъгълник и пресмята неговото лице.
+    Използвайте формулата за лице на триъгълник: area = a * h / 2.
+    Форматирате изхода до втория знак след десетичната запетая.
+Примерен вход и изход:
+    20
+    30
+    -> 300.00
+    15
+    35
+    -> 262.50
+    7.75
+    8.45
+    -> 32.74
+    1.23456
+    4.56789
+    -> 2.82
+*/
+package SoftUni.MoreExercises.FirstStepsInCoding;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class TriangleArea {
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        double a = setValue(0.0, Double.MAX_VALUE);
+        double h = setValue(0.0, Double.MAX_VALUE);
+
+        double area = calcArea(a, h);
+
+        out.printf("%.2f", area);
+    }
+
+    private static double calcArea(double a, double h) {
+        return a * h / 2;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+            /*
+            String[] required = { "" };
+            List<String> requiredList = List.of(required);
+            if (!requiredList.contains(value)){
+                out.print("Моля въведете един от следните избори: | ");
+                for (String thing : required)
+                    out.print(thing + " | ");
+                out.println();
+                return setValue(null, null);
+            } */
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка! Не сте въвели правилен тип. Възможности [int, double]");
+                    value = null;
+                    exit(1);
+                }
+            } catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    out.println("Моля въведете положително число между");
+                    return setValue(min, max);
+                }
+            }
+        }
+        return (T) value;
+    }
+}

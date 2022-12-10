0f0a5dcbf499529b35f28cd1ddf9d8340dc40889
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-10 19:47:13 +0200

    Update FootballKit.java

--- Exam/FootballKit.java
+++ Exam/FootballKit.java
@@ -52,24 +52,18 @@
 */
 package SoftUni.Exam;
 
-import static java.lang.System.exit;
 import static java.lang.System.out;
 import static java.lang.System.in;
-
 import java.util.Scanner;
-import java.util.List;
 
 public class FootballKit {
-    static int smallestInt = Integer.MIN_VALUE;
-    static int biggestInt = Integer.MAX_VALUE;
-    static double smallestDouble = -1 * Double.MAX_VALUE;
-    static double biggestDouble = Double.MAX_VALUE;
-
     static Scanner scanner = new Scanner(in);
 
     public static void main(String[] args) {
-        double priceOfTShirt = setValue(1.00, 1000.00);
-        double priceGoal = setValue(100.00, 10000.00);
+        double priceOfTShirt =
+            Double.parseDouble(scanner.nextLine());
+        double priceGoal =
+            Double.parseDouble(scanner.nextLine());
 
         checkIfGoalMet(priceOfTShirt, priceGoal);
     }
@@ -92,8 +86,11 @@ public class FootballKit {
         displayResult(priceForKitAfterDiscount, priceGoal);
     }
 
-    private static void displayResult(double priceForKitAfterDiscount, double priceGoal) {
-        double priceDifference = 
+    private static void displayResult(
+        double priceForKitAfterDiscount, 
+        double priceGoal
+    ) {
+        double priceDifference =
             priceGoal - priceForKitAfterDiscount;
         if (priceForKitAfterDiscount >= priceGoal) {
             out.println(
@@ -112,76 +109,5 @@ public class FootballKit {
                 priceDifference
             );
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> T setValue(T min, T max) {
-        Object value;
-        // out.println("Въведете :");
-
-        if (min == null && max == null) {
-            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
-            boolean isSpecChar = false;
-            value = scanner.nextLine();
-
-            for (int i = 0; i < ((String) value).length(); i++)
-                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
-                    isSpecChar = true;
-                    break;
-                }
-
-            if (isSpecChar) {
-                out.println("Моля въведете правилно наименование!");
-                return setValue(null, null);
-            }
-            /*
-            String[] required = { "" };
-            List<String> requiredList = List.of(required);
-            if (!requiredList.contains(value)){
-                out.print("Моля въведете един от следните избори: | ");
-                for (String thing : required)
-                    out.print(thing + " | ");
-                out.println();
-                return setValue(null, null);
-            } */
-        }
-        else {
-            try {
-                if (max instanceof Integer)
-                    value = Integer.parseInt(scanner.nextLine());
-                else if (max instanceof Double)
-                    value = Double.parseDouble(scanner.nextLine());
-                else {
-                    out.println("Грешка!");
-                    value = null;
-                    exit(1);
-                }
-            } catch (Exception e) {
-                out.println("Не сте въвели число. Пробвайте пак!");
-                return setValue(min, max);
-            }
-
-            if (max instanceof Integer) {
-                if ((int) value < (int) min || (int) value > (int) max) {
-                    if ((int) min == 0 && (int) max == biggestInt)
-                        out.println("Моля въведете положително число:");
-                    else
-                        out.printf("Моля въведете число между %s и %s:\n", min, max);
-
-                    return setValue(min, max);
-                }
-            }
-            if (max instanceof Double) {
-                if ((double) value < (double) min || (double) value > (double) max) {
-                    if ((double) min == 0 && (double) max == biggestDouble)
-                        out.println("Моля въведете положително число:");
-                    else
-                        out.printf("Моля въведете число между %s и %s:\n", min, max);
-
-                    return setValue(min, max);
-                }
-            }
-        }
-        return (T) value;
     }
 }

1d4e9cf1327d5d66ef20232d3d2cb327471fb150
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-16 17:55:02 +0200

    Create NumbersFrom1To10.java

---
+++ Conditional-Statements-Advanced-More-Exercises/NumbersFrom1To10.java
@@ -0,0 +1,33 @@
+/*
+Условие:
+    Напишете програма, която отпечатва числата от 1 до 10, по едно на ред.
+Примерен вход и изход:
+    (няма)
+    1
+    2
+    3
+    …
+    10
+*/
+package SoftUni.MoreExercises.ConditionalStatementsAdvanced;
+
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+
+public class NumbersFrom1To10 {
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        printNumbersFrom(1, 10);
+    }
+
+    private static void printNumbersFrom(int i, int i1) {
+        while (i<=i1) {
+            out.println(i);
+            i++;
+        }
+    }
+}

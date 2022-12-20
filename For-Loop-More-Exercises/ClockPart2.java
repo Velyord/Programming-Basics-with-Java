34694b17fb5d23b0fbd0027a18e4d1376b6f1b7f
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-20 14:13:05 +0200

    Create ClockPart2.java

---
+++ For-Loop-More-Exercises/ClockPart2.java
@@ -0,0 +1,42 @@
+/*
+Условие:
+    Напишете програма, която отпечатва часовете в денонощието от 0:0 до 23:59,
+    всеки на отделен ред. Часовете трябва да се изписват във формат "{час} : {минути}".
+Примерен вход и изход:
+    (няма вход) ->
+    0 : 0
+    0 : 1
+    0 : 2
+    0 : 3
+    0 : 4
+    0 : 5
+    0 : 6
+    0 : 7
+    0 : 8
+    0 : 9
+    0 : 10
+    ...
+    23 : 50
+    23 : 51
+    23 : 52
+    23 : 53
+    23 : 54
+    23 : 55
+    23 : 56
+    23 : 57
+    23 : 58
+    23 : 59
+*/
+package SoftUni.MoreExercises.ForLoop;
+
+import static java.lang.System.out;
+
+public class ClockPart2 {
+
+    public static void main(String[] args) {
+        for (int hours = 0; hours <= 23; hours++)
+            for (int minutes = 0; minutes <= 59; minutes++)
+                for (int seconds = 0; seconds <= 59; seconds++)     
+                    out.printf("%d : %d : %d\n", hours, minutes, seconds);
+    }
+}

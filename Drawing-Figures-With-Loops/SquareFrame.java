9c7ec18f56f1cf63e437d1aec5a06889b0821ab7
Yoan Velyord <117631417+Velyord@users.noreply.github.com>
2022-12-20 15:03:33 +0200

    Create SquareFrame.java

---
+++ Drawing-Figures-With-Loops/SquareFrame.java
@@ -0,0 +1,152 @@
+/*
+Условие:
+    Напишете програма, която чете цяло положително число n, въведено от потребителя,
+    и чертае на конзолата квадратна рамка с размер n * n като в примерите по-долу:
+Примерен вход и изход:
+    3 ->
+    + - +
+    | - |
+    + - +
+    4 ->
+    + - - +
+    | - - |
+    | - - |
+    + - - +
+    5 ->
+    + - - - +
+    | - - - |
+    | - - - |
+    | - - - |
+    + - - - +
+    6 ->
+    + - - - - +
+    | - - - - |
+    | - - - - |
+    | - - - - |
+    | - - - - |
+    + - - - - +
+*/
+package SoftUni.MoreExercises.DrawingFiguresWithLoops;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+import static java.lang.System.in;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class SquareFrame {
+    static int smallestInt = Integer.MIN_VALUE;
+    static int biggestInt = Integer.MAX_VALUE;
+    static double smallestDouble = -1 * Double.MAX_VALUE;
+    static double biggestDouble = Double.MAX_VALUE;
+    static int stringCount = 0;
+    static boolean requiredString = false;
+
+    static Scanner scanner = new Scanner(in);
+
+    public static void main(String[] args) {
+        int rows = setValue(0, biggestInt);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= rows; col++) {
+                if (row == 1 && (col == 1 || col == rows) ||
+                    row == rows && (col == 1 || col == rows))
+                    out.print("+ ");
+                else if (row != 1 && row != rows && (col == 1 || col == rows))
+                    out.print("| ");
+                else
+                    out.print("- ");
+            }
+            out.println();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T setValue(T min, T max) {
+        Object value;
+        // out.println("Въведете :");
+
+        if (min == null && max == null) {
+            String specialCharacters = "!#$%&'()*+,./:;<=>?@[]^_`{|}0123456789";
+            boolean isSpecChar = false;
+            value = scanner.nextLine();
+
+            for (int i = 0; i < ((String) value).length(); i++)
+                if (specialCharacters.contains(Character.toString(((String) value).charAt(i)))) {
+                    isSpecChar = true;
+                    break;
+                }
+
+            if (isSpecChar) {
+                out.println("Моля въведете правилно наименование!");
+                return setValue(null, null);
+            }
+
+            if (requiredString){
+                stringCount++;
+                String[] required = {};
+
+                if (stringCount == 1)
+                    required = new String[] {"Spring", "Summer", "Autumn", "Winter"};
+                if (stringCount == 2)
+                    required = new String[] {"Y", "N"};
+                if (stringCount > 2) {
+                    requiredString = false;
+                    return (T) value;
+                }
+
+                List<String> requiredList = List.of(required);
+
+                if (!requiredList.contains(value)){
+                    out.print("Моля въведете един от следните избори: \n| ");
+                    for (String thing : required)
+                        out.print(thing + " | ");
+                    out.println();
+
+                    stringCount--;
+                    return setValue(null, null);
+                }
+            }
+        }
+        else {
+            try {
+                if (max instanceof Integer)
+                    value = Integer.parseInt(scanner.nextLine());
+                else if (max instanceof Double)
+                    value = Double.parseDouble(scanner.nextLine());
+                else {
+                    out.println("Грешка!");
+                    value = null;
+                    exit(1);
+                }
+            }
+            catch (Exception e) {
+                out.println("Не сте въвели число. Пробвайте пак!");
+                return setValue(min, max);
+            }
+
+            if (max instanceof Integer) {
+                if ((int) value < (int) min || (int) value > (int) max) {
+                    if ((int) min == 0 && (int) max == biggestInt)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+            if (max instanceof Double) {
+                if ((double) value < (double) min || (double) value > (double) max) {
+                    if ((double) min == 0 && (double) max == biggestDouble)
+                        out.println("Моля въведете положително число:");
+                    else
+                        out.printf("Моля въведете число между %s и %s:\n", min, max);
+
+                    return setValue(min, max);
+                }
+            }
+        }
+
+        return (T) value;
+    }
+}
